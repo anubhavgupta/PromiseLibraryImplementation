485e2e1ee582c3e859e8473d782c46618a6df7da
Anubhav Gupta <anubhav200@gmail.com>
2016-09-04 21:38:30 +0530

    added custom test runner with its specs

--- Test.js
+++ Test.js
@@ -1,3 +1,42 @@
+// Test Runner
+function TestRunner(){
+    var $resultsContainer = document.getElementsByClassName("test-results")[0];
+    function addResult(name,result){
+        var $testName = document.createElement("p");
+        var $status = document.createElement("span");
+        $testName.innerText = name+" : ";
+        $status.innerText = result? "PASS": "FAIL";
+        $status.className = result? "PASS": "FAIL";
+        $testName.appendChild($status);
+        $resultsContainer.appendChild($testName);
+    }
+
+
+    function getExpect(name){
+        return function(a,b){
+            var result = (a === b);
+            addResult(name,result);
+        };
+    }
+
+    return function(name,fn){
+        fn.apply(null,[getExpect(name)])
+    };
+}
+
+var it = TestRunner();
+
+it("Test Runner should run",function(expect){
+    expect(true,true);
+});
+it("Test Runner should run after 1 sec",function(expect){
+    setTimeout(function(){
+        expect(true,true);
+    },1000);
+});
+
+
+
 var res, rej;
 var succ = function (val) {
     console.log("Pass", val);
